7d84a9b1d3ed1627a3820d23977944c1fae93345
placidenduwayo1 <placide.nd@gmail.com>
2023-09-20 14:27:32 +0200

    feat (Jenkinsfile) publishing test report

--- K8s-Kafka-AEPC-Back/k8s-kafka-aepc-clean-archi-bs-ms-employee/src/test/java/fr/placide/k8skafkaaepccleanarchibsmsemployee/K8sKafkaAepcCleanArchiBsMsEmployeeApplicationTests.java
+++ K8s-Kafka-AEPC-Back/k8s-kafka-aepc-clean-archi-bs-ms-employee/src/test/java/fr/placide/k8skafkaaepccleanarchibsmsemployee/K8sKafkaAepcCleanArchiBsMsEmployeeApplicationTests.java
@@ -3,7 +3,6 @@ package fr.placide.k8skafkaaepccleanarchibsmsemployee;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class K8sKafkaAepcCleanArchiBsMsEmployeeApplicationTests {
 
